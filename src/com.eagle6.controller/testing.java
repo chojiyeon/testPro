30be4591649270856616220b0ead156d3f96f6b9
user <user@DESKTOP-UL2U1T2>
2019-11-12 14:14:33 +0900

    추가 입력했습니다

--- src/com.eagle6.controller/testing.java
+++ src/com.eagle6.controller/testing.java
@@ -3,5 +3,6 @@ package com.eagle6.controller;
 public class testing {
     public static void main(String[] args) {
         System.out.println("Hello world");
+        System.out.println("제가 수정했습니다.");
     }
 }
