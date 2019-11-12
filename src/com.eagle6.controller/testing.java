241ce90d6b98b92ec49aebcdd4dd2959f4449d3e
user <user@DESKTOP-TEBR1F8>
2019-11-12 17:08:31 +0900

    chojiyeonBranch

--- src/com.eagle6.controller/testing.java
+++ src/com.eagle6.controller/testing.java
@@ -4,6 +4,6 @@ public class testing {
     public static void main(String[] args) {
         System.out.println("Hello world");
         System.out.println("Á¶Áö¿¬ branch");
-        System.out.println("1");
+        System.out.println("2");
     }
 }
